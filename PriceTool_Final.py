f888fb1d0cc46415bbc56dce6270e155e989beeb
PaleAvery <143568240+PaleAvery@users.noreply.github.com>
2025-07-29 11:32:58 +1200

    Add files via upload

--- PriceTool_Final.py
+++ PriceTool_Final.py
@@ -1,24 +1,42 @@
 #add all the stuff for functions -- step 1
 #-- This adds pretty decorations to stuff
 
-
+# this allows me to make my instructions variable to look cool
 def make_statement(statement, decoration):
     """ Emphasises headings by adding decoration
      at the start and end"""
     return f"{decoration * 3} {statement} {decoration * 3}"
-#-- this is the instructions i will use the yes no to ask whether they want do display it
+def int_check(question):
+    """Checks users enter an integer that is more than zero (or the 'xxx' exit code)"""
+
+    error = "oops - please enter an integer."
+
+    while True:
+
+        try:
+
+            response = int(input(question))
+
+
+            return response
+        except ValueError:
+            print(error)
+#-- this is the instructions I will use the yes no to ask whether they want to do display it
 def instructions():
     print(make_statement("instructions", "⚙️"))
 
     print('''
-First Enter Your Budget and then 
-the code will show what the best option is and ask to buy it 
-if you cant afford the item then the code will give you an ultimatum 
-and ask if you would want to buy the next best thing
-and if the user doesnt have enough for anything it kicks you out
-Ps: The Weight Will Be Displayed In grams.
+Welcome to The Price Tool
+This code will compare prices of which you can afford and
+Then Decide the best Item for your budget 
+So First Put in Your Budget 
+then it will display the whole list of items 
+and then show the Best thing you can Afford 
+Then You can pick Whether you would like
+To Buy It
 
     ''')
+
 
 #-- this is my number checker it checks for float numbers so not zero
 def num_check(question, num_type="float"):
@@ -43,7 +61,7 @@ def num_check(question, num_type="float"):
                 print(error)
         except ValueError:
             print(error)
-#-- this is a yes no checker it checks whether the user say yes or no and nothing else
+#-- this is a yes no checker it checks whether the user say yes or no and rejects anything else
 def yes_no_check(question):
     """Checks that users enter yes / y or no/n to a question"""
     #
@@ -98,13 +116,15 @@ print(make_statement("Price Tool", "🍴"))
 print()
 want_instructions = yes_no_check("do you want to see the instructions")
 print()
-
+# im using the yes no checker in order to see if the user wants to read the instructions if not it won't display
 if want_instructions == "yes":
     instructions()
 # add the questions for budget -- step 4
     #--Asks what the users budget is
 budget = num_check("What is Your Budget")
-#-- prints out the users budget in dollars in case you forgot
+# if name is exit code break out of loop
+
+#-- prints out the users budget in dollars ($)
 print(f" {budget:.2f}$ is your budget")
 #present a spread for the price tool items -- step 5
 #-- prints out the multiple different options and their category such as weight grams , weight kilos , cost $ , unit price $, User budget
@@ -134,7 +154,7 @@ if budget >= min(cost):
             unitprice.remove(x)
             weight.remove(x)
 
-    # Checks for items in unit price that arent the minimum and makes them 100 including cost and weight
+    # Checks for items in unit price that aren't the minimum and makes them 100 including cost and weight
     for i in range(len(cost)):
         if unitprice[i] > min(unitprice):
             unitprice[i] = 100
@@ -159,9 +179,9 @@ if budget >= min(cost):
     # tell what the best option is skip step 9   -- step 8
     ## https://www.datacamp.com/tutorial/python-tabulate used datacamp for ideas on how to display this
     # Import the tabulate module
-    if cost == 1:
+    if cost[0] == 1:
         name = "Apple"
-    elif cost == 5:
+    elif cost[0] == 5:
         name = "Chips"
     else:
         name = "Chocolate"
@@ -174,6 +194,7 @@ if budget >= min(cost):
     ]
     remain = budget - cost[0]
     # Creating a table with headers and a grid format
+    print("This is the best priced item i could find.")
     table = tabulate(
         data,
         headers=["Name", "Cost", "Weight","UnitPrice(Per Kilo)"],
@@ -183,9 +204,95 @@ if budget >= min(cost):
     print(table)
     buy = yes_no_check(f"would you like to buy {name} for {cost[0]:.2f}$  "
                        f"You Have {budget:.2f}$  ")
+    # if you say yes you will buy the thing and subtract the cost from your budget
     if buy == "yes":
         print(f"you have bought {name} for {cost[0]:.2f}$ and have"
-              f" S{remain:.2f}$ left ")
-    # say your poor -- step 9
+              f" {remain:.2f}$ left ")
+    else:
+        if budget >= 5:
+            option = yes_no_check(" Would you like to pick something else ")
+            if option == "yes":
+                from tabulate import tabulate
+
+                a = [['Chips', f'{chipswg:.2f}g', f'{chipswk:.2f}Kg', f'{chipsc:.2f}$', f'{chipsup:.2f}$',
+                      f'{budget:.2f}$'],
+                     ['Apple', f'{applewg:.2f}g', f'{applewk:.2f}Kg', f'{applec:.2f}$', f'{appleup:.2f}$'],
+                     ['Chocolate', f'{chocolatewg:.2f}g', f'{chocolatewk:.2f}Kg', f'{chocolatec:.2f}$',
+                      f'{chocolateup:.2f}$']]  # data
+                b = ['Name', 'Weight Grams', 'Weight Kilos', 'Cost[$]', 'Unit Price per kilo', 'User Budget']  # headers
+                print("this is the available products pick one From 1-3 ")
+                print(tabulate(a, headers=b, tablefmt="pipe"))
+            # im using a while true statement in order to loop my code until they pick from 1 to 3
+            while True:
+                # im using an int check to make the user type 1 -3 for options and using an else statement to loop the code
+                pick = int_check("pick something from 1 -3 ")
+                # this displays the chips graph and says you bought it
+                if pick == 1 and budget >= chipsc:
+                    data = [
+                        ["Chips", f"{chipsc:.2f}$", f"{chipswg:.2f}g", f"{chipsup:.2f}$"]
+
+                    ]
+                    remain = budget - cost[0]
+                    # Creating a table with headers and a grid format
+                    print("this is your item.")
+                    table = tabulate(
+                        data,
+                        headers=["Name", "Cost", "Weight", "UnitPrice(Per Kilo)"],
+                        tablefmt="pipe"
+                    )
+                    # this displays the table for the chips
+                    print(table)
+                    # this prints out that you bought the apple and shows how much money you have left
+                    print(f"you have bought the Chips for {chipsc:.2f}$ and now have {budget - chipsc :.2f}$ left")
+                    break
+                    # this is what shows after the user presses option three  and displays the apple you bought
+                elif pick == 2 and budget >= applec:
+                    data = [
+                        ["Chips", f"{applec:.2f}$", f"{applewg:.2f}g", f"{appleup:.2f}$"]
+
+                    ]
+                    remain = budget - cost[0]
+                    # Creating a table with headers and a grid format
+                    print("this is your item.")
+                    table = tabulate(
+                        data,
+                        headers=["Name", "Cost", "Weight", "UnitPrice(Per Kilo)"],
+                        tablefmt="pipe"
+                    )
+                    # this displays the table for the apple
+                    print(table)
+                    # this prints what you bought and how much money you have left
+                    print(f"you have bought the Apple for {applec:.2f}$ and now have {budget - applec :.2f}$ left")
+                    break
+                # this is what shows when you press option three it displays the graph for chocolate and then says It's what you bought
+                elif pick == 3 and budget >= chocolatec:
+                    data = [
+                        ["Chips", f"{chocolatec:.2f}$", f"{chocolatewg:.2f}g", f"{chocolateup:.2f}$"]
+
+                    ]
+                    remain = budget - cost[0]
+                    # Creating a table with headers and a grid format
+                    print("this is your item ")
+                    table = tabulate(
+                        data,
+                        headers=["Name", "Cost", "Weight", "UnitPrice(Per Kilo)"],
+                        tablefmt="pipe"
+                    )
+                    # this displays the table that was created
+                    print(table)
+                    # this prints what you bought and how much money you have left
+                    print(f"you have bought the Chocolate for {chocolatec:.2f}$ and now have {budget - chocolatec :.2f}$ left")
+                    break
+                elif pick == "xxx":
+                    print("you have quit")
+                    break
+                else:
+                    print("You either didn't pick something between 1 and 3 "
+                          "or you can't afford it try again  ")
+                 
+
+
+
+    # say you're poor -- step 9
 else:
-    print("you cant afford anything comeback with more money")
+    print("you cant afford anything  come back with more money")
